02f902a6f750b9a14fa329cb52051bfae1e80113
007a83 <007a83@gmail.com>
2020-11-29 20:07:24 -0800

    Update doge.js

--- assets/doge.js
+++ assets/doge.js
@@ -5,6 +5,6 @@
 		if ( e.keyCode == konami[state] ) state++;  
 		else state = 0;  
 		if ( state == 10 )  
-		  window.open ("https://www.youtube.com/embed/000al7ru3ms?autoplay=1&iv_load_policy=3&vq=hd1080","mywindow","menubar=1,resizable=1,width=350,height=250");
+		  window.open ("https://www.youtube.com/embed/000al7ru3ms?autoplay=1&disablekb=1&controls=0&loop=1&modestbranding=1&iv_load_policy=3&vq=hd1080","mywindow","menubar=1,resizable=1,width=350,height=250");
 		}, true);  
 	} 
